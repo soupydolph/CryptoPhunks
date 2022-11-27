001f08d1a9957d0430ae9fbbfa38d073f251e06a
soupydolph <soupydolph@proton.me>
2022-11-27 10:41:29 -0800

    adding basic layout and css

---
+++ notes.md
@@ -0,0 +1,11 @@
+Home
+ - intro
+ - links to socials
+
+
+- NLL
+- Knowledge base
+- Flywheel
+- Open source projects
+- History
+- Museum
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import phunk from './assets/phunk0939.svg'
+import 'normalize.css';
 import './App.css'
 
 function App() {
@@ -7,26 +8,29 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <div className="wrapper">
+        <div className="topBar">
+          <h1 className="phunkTitle">CRYPTOPHUNKS</h1>
+          <menu className="menu">
+            <p>About</p>
+            <p>Projects</p>
+            <p>History</p>
+          </menu>
+        </div>
+
+
+
+        <div className='intro-text-container'>
+          <h3>We are <span>innovators</span></h3>
+          <p>
+            Phunks are free-use, public domain Art, run entirely by decentralized community of artist and builders. There are no Devs, no Leaders, no Roadmaps, just Phunk Pham building cool stuff cause they care about Web3 and decentralization. ✊
+          </p>
+        </div>
+        <div className='intro-phunk-container'>
+          <img src={phunk} className="logo react" alt="React logo" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
