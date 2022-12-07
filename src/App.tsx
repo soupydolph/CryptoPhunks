c19250f992a523076be99dcce41f240d35b136e2
soupydolph <soupydolph@proton.me>
2022-12-06 18:38:48 -0800

    rebuilt the whole thing lol

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
-import phunk from './assets/phunk0939.svg'
-import 'normalize.css';
+
 import './App.css'
 
 import Menu from './components/Menu/Menu';
@@ -11,6 +10,7 @@ import Twitter from './components/Twitter';
 import Discord from './components/Discord';
 import Medium from './components/Medium';
 
+import phunk from './assets/phunk0939.svg'
 import Video from './assets/phunks-intro-vid.mp4'
 import NllLogo from './assets/nll-logo.svg'
 import AuctionHouseLogo from './assets/auction-logo.svg'
@@ -30,133 +30,164 @@ const adjectives = [
   'phunky'
 ]
 
+const phunkIpfsBaseUrl = 'https://middlemarch.mypinata.cloud/ipfs/QmcvdPd7Jai74e595Mgx2u6D8QZZ1TGSFC2EQQNayQVJL8/';
+const phunksToShow = 8;
+
 function App() {
-  const [adjIndex, setAdjIndex] = useState(0)
+  const [phunks, setPhunks] = useState<number[]>([])
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setAdjIndex(prev => {
+  //       const next = (prev + 1)
+
+  //       if (next === adjectives.length) {
+  //         return 0;
+  //       } else {
+  //         return (prev + 1)
+  //       }
+  //     })
+  //   }, 3000);
+  //   return () => clearInterval(interval);
+  // }, []);
+
+  const getRandomInt = (max: number) => {
+    return Math.floor(Math.random() * max);
+  }
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setAdjIndex(prev => {
-        const next = (prev + 1)
-
-        if (next === adjectives.length) {
-          return 0;
-        } else {
-          return (prev + 1)
-        }
-      })
-    }, 3000);
-    return () => clearInterval(interval);
-  }, []);
+    const phunkList = []
+    for (let i = 0; i < phunksToShow; i++) {
+      phunkList.push(getRandomInt(10000))
+    }
+    setPhunks(phunkList)
+  }, [])
 
   return (
-    <div className="App">
-      <div className="wrapper">
-        {/* Row 1: Top bar */}
-        <div className="row__top-bar">
-          <h1 className="phunkTitle phunk-gradient">CRYPTOPHUNKS</h1>
-          {/* <Menu /> */}
+    <>
+      <section className="section is-medium no-bottom-padding">
+        <div className="phunk-intro has-text-centered">
+          <h1 className="mb-6">CRYPTOPHUNKS</h1>
+          <h2 className="large-title">Be free, be Phunky</h2>
         </div>
+      </section>
 
-        {/* Row 2: Intro */}
-        <div className='row__intro__text-container'>
-          <h3 className="title">We are <span className="phunk-gradient">{adjectives[adjIndex]}</span></h3>
-          <p>
-            Phunks are free-use, public domain Art, run entirely by decentralized community of artist and builders.
-          </p>
-          <p>
-            There are no Devs, no Leaders, no Roadmaps, just Phunk Pham building cool stuff cause they care about Web3 and decentralization. ✊
-          </p>
-          <p>
-            You can learn more about Phunks at the <Link url="https://phunks.gitbook.io/" title="Knowledge Base" />
-          </p>
-          <p>
-            You can buy a Phunk at <Link url="https://notlarvalabs.com/" title="Not Larva Labs" /> or the <Link url="https://phunks.auction/" title="Auction House" /> for 0% fees!
-          </p>
-          <Button link="https://notlarvalabs.com/" text="buy a phunk" />
-          <div className="phunkySocial">
-            <a href="https://twitter.com/CryptoPhunksV2" target="_blank" rel="noopener noreferrer">
-              <Twitter />
-            </a>
-            <a href="http://discord.gg/phunks" target="_blank" rel="noopener noreferrer">
-              <Discord />
-            </a>
-            <a href="https://eggphunk.medium.com/" target="_blank" rel="noopener noreferrer">
-              <Medium />
-            </a>
+      <section className="section is-medium no-bottom-padding">
+        <div className="columns is-multiline is-mobile">
+          {phunks.map((phunk, idx) => {
+            return (
+              <div key={`phunk-image-${idx}`} className="column is-one-quarter">
+                <img src={`${phunkIpfsBaseUrl}${phunk}.png`} alt="" />
+              </div>
+            )
+          })}
+        </div>
+      </section>
+
+      <section className="section is-medium no-bottom-padding">
+        <div className="columns is-mobile is-centered">
+          <div className="column is-half has-text-centered">
+            <p className='has-text-white is-size-4'>
+              Phunks are free-use, public domain Art, run entirely by decentralized community of artist and builders.
+            </p>
+            <br></br>
+            <p className='has-text-white is-size-4'>
+              There are no Devs, no Leaders, no Roadmaps, just Phunk Pham building cool stuff cause they care about
+              Web3 and decentralization. ✊
+            </p>
+            <br></br>
+            <p className='has-text-white is-size-4'>
+              You can learn more about Phunks at the Knowledge Base
+              You can buy a Phunk at <Link
+                url="https://notlarvalabs.com/"
+                title="Not Larva Labs"
+              /> or the <Link
+                url="https://notlarvalabs.com/"
+                title="Auction House"
+              /> for 0% fees!
+            </p>
+            <br></br>
+            <Button
+              link="https://notlarvalabs.com/"
+              text="buy a phunk"
+              wide={true}
+            />
           </div>
         </div>
-        <div className='row__intro__phunk-container'>
-          <img src={phunk} className="phunk" alt="Phunky" />
+      </section>
+
+      <section className="section is-medium">
+        <h2 className="large-title mb-6">Phunky Projects</h2>
+        <div className="columns side-by-side">
+          <div className="column side-by-side-col">
+            <h3 className='has-text-white is-size-2'>Not Larva Labs</h3>
+            <p className='has-text-white is-size-4'>
+              NotLarvaLabs (NLL) is a community built royalty-free Marketplace for buying and
+              selling CryptoPhunks, built on top of it's own custom smart contract with 0% trading fees.
+              Idea came to life after two Big Corporations (LarvaLabs and OpenSea) decided to censor CryptoPhunks.
+            </p>
+            <Button
+              link="https://notlarvalabs.com/"
+              text="Visit"
+            />
+          </div>
+          <div className="column side-by-side-col side-by-side-col--image">
+            <img src={NllLogo} alt="" />
+          </div>
         </div>
-      </div>
 
-      <SideBySide
-        title="So, what happened?"
-        image={Video}
-        reverse={true}
-        media="video"
-        shouldHideLink={true}
-      >
-        <div>
-          <p>CryptoPhunks (Phunks) is a Non-Fungible Token (NFT) collection on the Ethereum blockchain.</p>
-          <p>
-            The project was created in June 2021 by a group of anonymous* developers to poke fun (parody) at the high-brow, pompous group of people that were reflecting the “old-school” rules of art into this new frontier of Web3.
-          </p>
-          <p>
-            What they did is they took all 10'000 CryptoPunks and flipped them the other way, they are the same 1:1 just facing Left.
-          </p>
+        <div className="columns side-by-side">
+          <div className="column side-by-side-col side-by-side-col--image">
+            <img className='auction-image' src={AuctionHouseLogo} alt="" />
+          </div>
+          <div className="column side-by-side-col">
+            <h3 className='has-text-white is-size-2'>CryptoPhunks Auction House</h3>
+            <p className='has-text-white is-size-4'>
+              CryptoPhunks Auction House was built by Chopper and Kenobi with the purpose of facilitating perpetual
+              auctions for CryptoPhunks held in the Treasury Vault. All Ethereum collected from settled auctions will
+              remain in the treasury under direction of the community.
+            </p>
+            <Button
+              link="https://phunks.auction/"
+              text="Visit"
+            />
+          </div>
         </div>
-      </SideBySide>
 
-      <SideBySide
-        title="NotLarvaLabs"
-        link="https://notlarvalabs.com/"
-        image={NllLogo}
-        reverse={false}
-        media="image"
-      >
-        <p>
-          NotLarvaLabs (NLL) is a community built royalty-free Marketplace for buying and selling CryptoPhunks, built on top of it's own custom smart contract with 0% trading fees.
-          Idea came to life after two Big Corporations (LarvaLabs and OpenSea) decided to censor CryptoPhunks.
-        </p>
-      </SideBySide>
+        <div className="columns side-by-side">
+          <div className="column side-by-side-col">
+            <h3 className='has-text-white is-size-2'>FlyWheel</h3>
+            <p className='has-text-white is-size-4'>
+              The FlyWheel is a DAPP that appraises mid- and top-tier CryptoPhunks and gives holders the option
+              to sell them to the Treasury at a small discount to fair market value in a trustless, fair, and automatic way.
+            </p>
+            <Button
+              link="https://www.phunks.pro/"
+              text="Visit"
+            />
+          </div>
+          <div className="column side-by-side-col side-by-side-col--image">
+            <img className='flywheel-image' src={FlyWheelLogo} alt="" />
+          </div>
+        </div>
 
-      <SideBySide
-        title="CryptoPhunks Auction House"
-        link="https://phunks.auction/"
-        image={AuctionHouseLogo}
-        reverse={true}
-        customClass="auction_img"
-        media="image"
-      >
-        <p>
-          CryptoPhunks Auction House was built by Chopper and Kenobi with the purpose of facilitating perpetual auctions for CryptoPhunks held in the Treasury Vault. All Ethereum collected from settled auctions will remain in the treasury under direction of the community.
-        </p>
-      </SideBySide>
-
-      <SideBySide
-        title="FlyWheel"
-        link="https://www.phunks.pro/"
-        image={FlyWheelLogo}
-        reverse={false}
-        media="image"
-      >
-        <p>
-          The FlyWheel is a DAPP that appraises mid- and top-tier CryptoPhunks and gives holders the option to sell them to the Treasury at a small discount to fair market value in a trustless, fair, and automatic way.
-        </p>
-      </SideBySide>
-
-      <SideBySide
-        title="Phunks Knowledge Base"
-        link="https://phunks.gitbook.io/"
-        image={KnowledgeBaseLogo}
-        reverse={true}
-        media="image"
-        customClass="knowledge_img"
-      >
-        <p>The museum of all things Phunky! Take a deep dive into the history of the phunks.</p>
-      </SideBySide>
-    </div>
+        <div className="columns side-by-side">
+          <div className="column side-by-side-col side-by-side-col--image">
+            <img className='flywheel-image' src={KnowledgeBaseLogo} alt="" />
+          </div>
+          <div className="column side-by-side-col">
+            <h3 className='has-text-white is-size-2'>Phunks Knowledge Base</h3>
+            <p className='has-text-white is-size-4'>
+              The museum of all things Phunky! Take a deep dive into the history of the phunks.
+            </p>
+            <Button
+              link="https://www.phunks.pro/"
+              text="Visit"
+            />
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,8 @@
 import styles from './Button.module.css'
 
-const Button = (props: { link: string, text: string, alt?: string }) => {
+const Button = (props: { link: string, text: string, alt?: string, wide?: boolean }) => {
   return (
-    <a className={styles.button} href={props.link} target="_blank" rel="">{props.text}</a>
+    <a className={`${styles.button}  ${props.wide ? styles['wide-button'] : ''}`} href={props.link} target="_blank" rel="">{props.text}</a>
   )
 }
 
